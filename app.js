9373864de46c43cc8fb78630034397487465c5a8
larsRothaus <larsRothaus@gmail.com>
2019-11-26 15:05:21 +0100

    Co-authored-by: Daniel Hedenius <daniel.hedenius@gmail.com>

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
-let hello = "Daniel";
-let fuu = "bar";
-let t = "asdf";
+const express = require('express');
+const application = express();
+const cors = require('cors');
+const fs = require('fs');
+const port = 8888;
+
+application.use(express.json());
+application.use(cors());
+
+
+let isMockAds = false;
+let manifest1 = fs.readFileSync(`./Test/testManifest.m3u8`);
+let manifest2 = fs.readFileSync(`./Test/nxtManifest.m3u8`);
+
+application.get('/v1/manifest/master/:stream.m3u8', (request, response) => {
+  let m = isMockAds ? manifest2 : manifest1;
+  response.set({
+    'Content-Length': m.length,
+    'Content-type': 'application/x-mpegURL',
+  });
+  response.status(200).send(m);
+});
+application.get('/v1/manifest/variant/:stream.m3u8', (request, response) => {
+  console.log('Variant stream:', request.params.stream);
+  response.status(200).send('');
+});
+application.get('/go', (request, response) => {
+  isMockAds = !isMockAds
+
+  response.status(200).send('');
+});
+
+application.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
+});
